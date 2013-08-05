141d262dca7bc60b4a8987ab0c6e660262556993
Lucas Brendel <lucasbrendel@live.com>
2013-08-05 06:58:08 -0500

    Added comments

--- Text/Ruby/reverse_string.rb
+++ Text/Ruby/reverse_string.rb
@@ -1,6 +1,7 @@
-#TODO create documentation
+# Set 'string' to the value of each letter in the alphabet
 string = 'abcdefghijklmnopqrstuvwxyz'
 
+# Display the reversed string in the command window
 puts string.reverse
 
 #TODO create other alternatives to reverse string
